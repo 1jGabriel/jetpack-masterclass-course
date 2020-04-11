408a211c3f6df81f70b2da4a6e03141aff55fec8
João Sousa <joaogabriel.sousa@beblue.com.br>
2020-04-11 13:39:37 -0300

    Navigation Section - Part 15 - Navigation Funcionality - Added actions call on buttons and setup back arrow on toolbar to handle navigation

--- app/src/main/java/com/aupp/view/DetailFragment.kt
+++ app/src/main/java/com/aupp/view/DetailFragment.kt
@@ -1,23 +1,29 @@
 package com.aupp.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.aupp.R
+import kotlinx.android.synthetic.main.fragment_detail.*
 
-/**
- * A simple [Fragment] subclass.
- */
 class DetailFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonList.setOnClickListener {
+            Navigation.findNavController(it)
+                .navigate(DetailFragmentDirections.actionListFragment())
+        }
     }
 }
--- app/src/main/java/com/aupp/view/MainActivity.kt
+++ app/src/main/java/com/aupp/view/MainActivity.kt
@@ -1,13 +1,30 @@
 package com.aupp.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.aupp.R
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        navController = Navigation.findNavController(this, R.id.fragment)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
